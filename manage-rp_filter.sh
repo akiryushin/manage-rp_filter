4cd200ec9549267313a72a817cf5e3f7e5c5e98d
akiryushin <aakiryushin@gmail.com>
2021-08-10 00:44:18 +0300

    Initial script that manages rp_filter

---
+++ manage-rp_filter.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CONFIG_FILE='/etc/sysctl.d/90-disable-rp_filter.conf'
+INTERFACES=(all eth0)
+readonly STATUS='0'
+
+log () {
+    local MESSAGE="${@}"
+    logger -t "$(basename ${0})" "${MESSAGE}"
+}
+
+getstatus () {
+    local INTERFACE_NAME="${1}"
+    sysctl -a --ignore |& grep \\${INTERFACE_NAME}.rp_filter | awk '{ print $3 }'
+}
+
+log 'Starting change status of rp_filter'
+log 'Checking file if exists'
+# Delete config file if already exists
+if [[ -f "${CONFIG_FILE}" ]]
+then
+    log 'Delete config file'
+    rm ${CONFIG_FILE}
+fi
+
+# Change parameter state
+for INTERFACE_NAME in ${INTERFACES[@]}
+do
+    sysctl -a --ignore |& grep \\${INTERFACE_NAME}.rp_filter &> /dev/null
+    if [[ $? = 0 ]]
+    then
+        NET_CONFIG="$(sysctl -a --ignore |& grep \\${INTERFACE_NAME}.rp_filter | awk '{ print $1 }')"
+        log "Print current state of ${INTERFACE_NAME}.rp_filter: $(getstatus ${INTERFACE_NAME})"
+        sysctl -w "${NET_CONFIG}=${STATUS}" 2&> /dev/null
+        echo "${NET_CONFIG}=${STATUS}" >> ${CONFIG_FILE}
+        log "Print new state of ${INTERFACE_NAME}.rp_filter after changes: $(getstatus ${INTERFACE_NAME})"
+    fi
+done
+log 'Updated states'
+exit 0
